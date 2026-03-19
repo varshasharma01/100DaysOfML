e532821b748e17b6245ea169f1f8060edcf05b32
varshasharma01 <varshasinbox1@gmail.com>
2026-03-19 14:07:51 +0530

    ss

--- Practice/standardscaling.py
+++ Practice/standardscaling.py
@@ -8,11 +8,15 @@ class standardscaling:
         self.standard_deviation = 0    
         
     def fit(self, X):
-        self.mean = np.mean(X, axis=0)
-        self.standard_deviation = np.std(X, axis=0)
+        self.mean = np.mean(X)
+        self.standard_deviation = np.std(X)
+        return self.mean, self.standard_deviation
     
     def transform(self, X):
-        return (X - self.mean)/self.standard_deviation
+        if self.standard_deviation == 0:
+            return (X-self.mean)
+        else:
+            return (X-self.mean)/self.standard_deviation
         
     
     def fit_transform(self, X):
@@ -21,18 +25,13 @@ class standardscaling:
     
 
 df = pd.read_csv("/home/varsha/PracticePython/100DaysOfMl/Practice/SalaryData.csv")
-X = df['Age'].values.reshape(-1,1)
+
+X = df['Age']
+# print(X[3])
+# X = [100,120,130,150]
 
 standardscaler = standardscaling()
 X_scaled = standardscaler.fit_transform(X)
 
 print(X_scaled)
-
-
-
-
-        
-        
-
-
 
